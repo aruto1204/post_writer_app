888c69a1159b16d7f42ebab503a5e34b9e33d254
ryoma <116142999+aruto1204@users.noreply.github.com>
2025-05-08 14:22:25 +0900

    nextconfig修正

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,6 @@ import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
   images: {
     unoptimized: true,
   },
